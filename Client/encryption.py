438393667c6cd66e27c5adb978142db8ae8daa4d
TalTri1 <tal.tri1999@gmail.com>
2024-11-30 11:33:37 +0200

    Changing git ignore

--- Client/encryption.py
+++ Client/encryption.py
@@ -5,17 +5,18 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
 
-# Define file paths
-private_key_file = 'private_key.pem'
-public_key_file = 'public_key.pem'
+# Define file paths in the Client folder
+private_key_file = 'Client/private_key.pem'
+public_key_file = 'Client/public_key.pem'
 
 # Define a password for encryption
 password = b"your-secure-password"
 
+
 # Function to generate and save keys
 def generate_and_save_keys():
     # Generate an ECC private key
-    private_key = ec.generate_private_key(
+    private_key_fl = ec.generate_private_key(
         ec.SECP256R1()
     )
 
@@ -33,14 +34,14 @@ def generate_and_save_keys():
     key = kdf.derive(password)
 
     # Serialize and save the private key with encryption
-    private_key_pem = private_key.private_bytes(
+    private_key_pem = private_key_fl.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.PKCS8,
         encryption_algorithm=serialization.BestAvailableEncryption(key)
     )
 
     # Generate the public key from the private key
-    public_key = private_key.public_key()
+    public_key = private_key_fl.public_key()
 
     # Serialize and save the public key
     public_key_pem = public_key.public_bytes(
@@ -49,14 +50,15 @@ def generate_and_save_keys():
     )
 
     # Write the private key to a file
-    with open(private_key_file, 'wb') as private_file:
-        private_file.write(private_key_pem)
+    with open(private_key_file, 'wb') as private_key_fl:
+        private_key_fl.write(private_key_pem)
 
     # Write the public key to a file
-    with open(public_key_file, 'wb') as public_file:
-        public_file.write(public_key_pem)
+    with open(public_key_file, 'wb') as public_key_fl:
+        public_key_fl.write(public_key_pem)
 
     return private_key_pem, public_key_pem
+
 
 # Check if the key files exist
 if os.path.exists(private_key_file) and os.path.exists(public_key_file):
