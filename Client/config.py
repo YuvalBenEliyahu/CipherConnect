2e44b7a4f251203d36616580d658cf15e189bfcb
yuval <yuval1998@gmail.com>
2024-12-21 11:55:07 +0200

    fix CLIENT_PUBLIC_KEY path

--- Client/config.py
+++ Client/config.py
@@ -5,5 +5,5 @@ ENCODE = 'utf-8'  # Encoding for the communication
 TIME_STAMP_FORMAT = "%Y-%m-%d %H:%M:%S"  # Time stamp format for messages
 CLIENT_CHAT_TABLE = "client_chats"  # Table name for client chat messages
 CLIENT_PRIVATE_KEY = "private_key.pem"  # Table name for client private key
-CLIENT_PUBLIC_KEY = "server_public_key.pem"  # Table name for client public key
+CLIENT_PUBLIC_KEY = "public_key.pem"  # Table name for client public key
 SERVER_PUBLIC_KEY = "server_public_key.pem"  # Table name for server public key
